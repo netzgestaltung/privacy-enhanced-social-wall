1ba6539c1c31d2fb5c03919802704b1015386f23
netzgestaltung <t.fellinger@netzgestaltung.at>
2018-08-10 09:56:44 +0200

    Create pesf-wall.php

---
+++ pesf-wall.php
@@ -0,0 +1,90 @@
+
+// require_once PESF_INCLUDE_DIR . 'image-cache/ImageCache.php';
+require_once 'includes/class-social-post.php';
+require_once 'includes/class-local-social-image.php';
+
+// Filter content for Twitter usernames and hashtags
+// @see: https://stackoverflow.com/a/37853639/1165121
+function pesf_social_content($content){
+  $content = apply_filters('the_content', $content);
+  
+  // Usernames searching for "@"
+  $pattern_at = '/(?<!=|\b|&)@([a-z0-9_]+)/i';
+  $replace_at = '<a href="http://www.twitter.com/$1" target="_blank" rel="nofollow">@$1</a>';
+  $content = preg_replace($pattern_at, $replace_at, $content);
+  
+  // Hashtags searching for "#"
+  $pattern_hash = '/(?<!=|\b|&)#([a-z0-9_]+)/i';
+  $replace_hash = '<a href="http://twitter.com/hashtag/$1" target="_blank" rel="nofollow">#$1</a>';
+  $content = preg_replace($pattern_hash, $replace_hash, $content);
+  
+  return $content;
+}
+
+function get_pesf_wall(){
+  $wall = '';
+  $services = array(
+    'results' => array(),
+    'facebook' => array(
+      'shortname' => 'fb',
+      'slug' => 'facebook',
+      'label' => 'Facebook',
+    ),
+    'twitter' => array(
+      'shortname' => 'tw',
+      'slug' => 'twitter',
+      'label' => 'Twitter',
+    ),
+    'instagram' => array(
+      'shortname' => 'ins',
+      'slug' => 'instagram',
+      'label' => 'Instagram',
+    ),
+    'pinterest' => array(
+      'shortname' => 'pi',
+      'slug' => 'pinterest',
+      'label' => 'Pinterest',
+    ),
+    'youtube' => array(
+      'shortname' => 'yt',
+      'slug' => 'youtube',
+      'label' => 'Youtube',
+    ),
+  );  
+  $eol = "\r\n";
+  
+  foreach ( $services as $service_name => $service_data ) {
+    if ( get_option('pesf_' . $service_data['slug'] . '_enable') == 'y' ) {
+      include PESF_INCLUDE_DIR . '/services/' . $service_data['slug'] . '/' . $service_data['slug'] . '.php';
+
+      foreach ( $service_raw as $service_post ) {
+        $services['results'][] = new Social_Post($service_name, $service_post);
+      }
+    }
+  }
+  if ( !empty($services['results']) ) {
+    $wall .= '<div class="social-wall">';
+    foreach ( $services['results'] as $service_result ) {
+      $wall .= '<div class="wall-item ' . $service_result->type . '">' . $eol;
+      if ( isset($service_result->img) ) {
+        $wall_image = new Local_Social_Image($service_result->img);
+        if ( $wall_image->src ) {
+          $wall .= '<a class="wall-image" href="' . $service_result->url . '" target="_blank"><img src="' . $wall_image->src . '" alt="" /></a>' . $eol;
+        }
+      }
+      if ( isset($service_result->name) ) {
+        $wall .= '<h3 class="wall-title">' . $service_result->name . '</h3>' . $eol;
+      }
+      if ( isset($service_result->text) ) {
+        $wall .= '<div class="wall-content">' .  apply_filters('social_content', $service_result->text) . '</div>' . $eol;
+      }
+      $wall .= '</div>' . $eol;
+    }
+    $wall .= '</div>' . $eol;
+  }
+  // return $wall;
+  return $wall;
+}
+function pesf_wall(){
+  echo get_pesf_wall();
+}
